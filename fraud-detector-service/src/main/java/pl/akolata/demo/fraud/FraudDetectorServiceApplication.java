dd94aa1c62f7a4d7c4d335da129032fe6c849fa2
Aleksander Kolata <olek.tbg.konta@gmail.com>
2020-09-27 17:13:57 +0200

    fraud-detector-service added (API producer)

---
+++ fraud-detector-service/src/main/java/pl/akolata/demo/fraud/FraudDetectorServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.akolata.demo.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectorServiceApplication.class, args);
+	}
+
+}
---
+++ fraud-detector-service/src/test/java/pl/akolata/demo/fraud/FraudDetectorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akolata.demo.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
