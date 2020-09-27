1c1a641ea33e6917c605f9c3aa7fd5a8aecff77e
Aleksander Kolata <olek.tbg.konta@gmail.com>
2020-09-27 17:17:38 +0200

    loan-service added (API consumer)

---
+++ loan-service/src/main/java/pl/akolata/demo/loan/LoanServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.akolata.demo.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanServiceApplication.class, args);
+	}
+
+}
---
+++ loan-service/src/test/java/pl/akolata/demo/loan/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akolata.demo.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
